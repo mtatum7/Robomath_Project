ff883211ddabe82796a148625cc09a598d02416f
Vasu Agrawal <vasu_agrawal@yahoo.com>
2018-12-01 19:40:37 -0500

    drawing script in matlab

---
+++ draw_volumes.m
@@ -0,0 +1,19 @@
+load('outputs/twist_-180.mat');
+
+thresh = 0.2;
+occ = volumes;
+occ(occ < thresh) = 0;
+occ(occ >= thresh) = 1;
+occ = logical(occ);
+
+[x, y, z] = meshgrid(1:size(occ, 1), 1:size(occ, 2), 1:size(occ, 3));
+
+x = x(:);
+y = y(:);
+z = z(:);
+
+ptcloud = pointCloud([x(occ), y(occ), z(occ)]);
+
+hold on;
+pcshow(ptcloud, 'MarkerSize', 1000);
+pcshow([x(~occ), y(~occ), z(~occ)], [1, 0, 0]);
