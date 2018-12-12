03ba0ae958b21e79d08c5d598709e76c088c130d
Vasu Agrawal <vasu_agrawal@yahoo.com>
2018-12-12 14:27:15 -0500

    new plotting code

--- draw_volumes.m
+++ draw_volumes.m
@@ -1,19 +1,23 @@
-load('outputs/twist_-180.mat');
+clear;
+close;
 
-thresh = 0.2;
-occ = volumes;
-occ(occ < thresh) = 0;
-occ(occ >= thresh) = 1;
-occ = logical(occ);
+load('generated/gen_0999.mat');
+% volumes = upsample(volumes);
 
-[x, y, z] = meshgrid(1:size(occ, 1), 1:size(occ, 2), 1:size(occ, 3));
+[x, y, z] = meshgrid(1:size(volumes, 1), 1:size(volumes, 2), 1:size(volumes, 3));
 
 x = x(:);
 y = y(:);
 z = z(:);
 
-ptcloud = pointCloud([x(occ), y(occ), z(occ)]);
+volumes(volumes > 1) = 1;
+volumes(volumes < 0) = 0;
 
-hold on;
-pcshow(ptcloud, 'MarkerSize', 1000);
-pcshow([x(~occ), y(~occ), z(~occ)], [1, 0, 0]);
+colormap(flipud(parula));
+sizedata = volumes(:) * 100;
+sizedata(sizedata == 0) = NaN;
+scatter3(x, y, z, 'SizeData', sizedata, 'MarkerFaceColor', 'flat', 'MarkerEdgeColor', 'k', 'CData', volumes(:));
+axis 'equal'
+
+% pc = pointCloud([x, y, z], 'Intensity', volumes(:));
+% pcshow(pc, 'MarkerSize', 1000);
